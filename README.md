9c1ae0abb7c7a4bb1d156be07a12197e343f2dc1
root <root@raspberrypi>
2020-02-22 22:11:22 +0000

    created install script, uses s-nail instead of heirloom-mailx, uses /opt and not the home dir anymore

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Small programs and scripts for hackerspace infrastructure and automation
 ## SuperScanner
 A Raspberry Pi 3 with a Flatbed Scanner to send scans via button pushing to a mailadress.
 It's also connected to a USB LCD display (also in this repo) that shows the current status.
-[There's a German tutorial to build one yourself](https://wiki.das-labor.org/w/Projekt/Server_Scripte#SuperScanner )
+
+Just clone this repo to `/opt` in your fresh raspbian instance and execute `init_pi.sh`.
---
+++ init_pi.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo '==> install needed packages'
+apt install console-data scanbd sane imagemagick git s-nail vim
+echo '==> set keyboard to de-latin1'
+loadkeys de-latin1
+echo '==> comment any not needed scanner types and "net"'
+echo '==> default: canon, canon630p, canon_dr, canon_pp, plustek, plustek_pp'
+echo '==> editor gets opened in 3 seconds'
+sleep 3s
+vim /etc/sane.d/dll.conf
+echo '==> copying from "sane.d" to "scanbd"'
+cp -R /etc/sane.d/* /etc/scanbd
+echo '==> creating "/home/pi/prints'
+chown pi:pi -R /home/pi/prints
+mkdir /home/pi/prints
+cp -R /opt/labor-scripts/superscanner/scanbd/scanbd.conf /etc/scanbd/scanbd.conf
+echo '==> in 10 seconds we will test the scanning'
+sleep 5s
+echo '...4'
+sleep 1s
+echo '...3'
+sleep 1s
+echo '...2'
+sleep 1s
+echo '...1'
+sleep 1s
+echo '...0'
+sudo -u pi bash /opt/labor-scripts/superscanner/scanbd/scan.script
+echo '==> sending mail'
+sudo -u pi bash /opt/labor-scripts/superscanner/scanbd/email.script
+echo '==> creating startup-display service'
+cp -R /opt/labor-scripts/superscanner/startup-display.service /etc/systemd/system/
+systemctl enable startup-display.service
+echo '==> please reboot'
